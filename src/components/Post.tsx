1a6ea2f8659d2bca987c96b18fbd201abfbdcf2e
Marcelo García Prieto <magarciaprieto@gmail.com>
2021-06-17 02:33:52 -0300

    Install FontAwesome / Refactor Post Component / Add Footer Layout

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react';
+import { faThumbsUp, faShare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function Post() {
+const style = {
+  backgroundColor: '#fff',
+  padding: '10px 15px',
+  border: '1px solid #ddd',
+}
+
+interface IPostProps {
+  image: string
+}
+
+export default function Post ({image}: IPostProps) {
   return (
-     <div> This is a post !</div>
-  )
+    <div style={style}>
+      <img src={image} alt='' />
+      <div style={{ display: 'flex', backgroundColor: '#eee', marginLeft: '-15px', marginBottom: '-10px', width: 'calc(100% + 30px)'}}>
+        <div style={{ flex: 1, textAlign: 'center', padding: '10px 15px', cursor: 'pointer'}}><FontAwesomeIcon icon={faThumbsUp} /> Like </div>
+        <div style={{ flex: 1, textAlign: 'center', padding: '10px 15px', cursor: 'pointer'}}><FontAwesomeIcon icon={faShare} /> Compartir </div>
+      </div>
+    </div>
+  );
 }
--- src/containers/NewsFeed/index.tsx
+++ src/containers/NewsFeed/index.tsx
@@ -6,7 +6,9 @@ import Container from '../../components/Container'
 export default function NewsFeed() {
   return (
     <Container>
-      <Post />
+      <div style={{ margin: '0 auto' }}>
+        <Post image='http://placekitten.com/300/200' />
+      </div>
     </Container>
   )
 }
