39747dc438888ee0b5abd876667d3214e31640a1
Marcelo García Prieto <magarciaprieto@gmail.com>
2021-06-17 01:44:58 -0300

    Add NewsFeed + Post components

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { Route } from 'react-router-dom';
 //Components
 import Login from './containers/Auth/Login';
 import Register from './containers/Auth/Register';
+import NewsFeed from './containers/NewsFeed';
 
 function App() {
   return (
     <div> 
       <Route exact={true} path='/' component={Login} />
       <Route exact={true} path='/register' component={Register} />
+      <Route exact={true} path='/app/newsfeed' component={NewsFeed} />
     </div>
   );
 }
---
+++ src/containers/NewsFeed/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Post from '../../components/Post'
+import Container from '../../components/Container'
+
+export default function NewsFeed() {
+  return (
+    <Container>
+      <Post />
+    </Container>
+  )
+}
