da69b9111f09f018ca907451dc375f1cf0ce6685
Marcelo García Prieto <magarciaprieto@gmail.com>
2021-06-16 21:57:06 -0300

    Add Card Component

---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const style = {
+  backgroundColor: '#fff',
+  padding: '10px 15px',
+  border: '1px solid #eee',
+}
+
+interface ICardProps {
+  children: string
+}
+
+export default function Card ({children}: ICardProps) {
+  return (
+    <div style={style}>
+      {children}
+    </div>
+  );
+}
