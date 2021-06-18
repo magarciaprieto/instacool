b44fa7605209b139085960e86f41d9c1651e99a4
Marcelo García Prieto <magarciaprieto@gmail.com>
2021-06-17 22:41:39 -0300

    Add Redux + Thunks

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import * as reducers from './ducks'
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(combineReducers({
+  ...reducers,
+}), applyMiddleware(thunk));
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 reportWebVitals();
