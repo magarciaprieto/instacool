b91bf4d83621dde643d60eeb14108188309de17e
Marcelo García Prieto <magarciaprieto@gmail.com>
2021-06-23 00:38:08 -0300

    Create users profile / Modify firestore rules for users

--- src/ducks/Users.ts
+++ src/ducks/Users.ts
@@ -1,5 +1,6 @@
 import { Dispatch } from "redux";
 import { IServices } from '../services';
+import { db } from "../services/firebase";
 
 export interface ILogin {
   email: string
@@ -16,6 +17,10 @@ export const login = ({ email, password }: ILogin) =>
   }
 
 export const register = ({ email, password }: ILogin) => 
-  async (dispatch: Dispatch, getState: () => any, { auth }: IServices) => {
-    await auth.createUserWithEmailAndPassword(email, password);
+  async (dispatch: Dispatch, getState: () => any, { auth, db }: IServices) => {
+    const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+    const { user } = userCredential
+    const id = user ? user.uid : undefined
+    const doc = db.collection('users').doc(id)
+    await doc.set({ role: 'user' })
   };
